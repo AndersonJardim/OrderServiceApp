b056dcb624a7af5c9f42a1fd83443a1a56f30a06
Anderson Jardim <andersonjardim@gmail.com>
2025-07-16 17:24:14 -0300

    Adicionado Controllers de Orders

---
+++ OrderServiceAPP/OrderServiceApp.API/Controlles/OrdersControlles.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderServiceApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        /// <summary>
+        /// Serviço para cadastro de pedidos
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
+        /// <summary> 
+        /// Serviço para atualiação de pedidos. 
+        /// </summary> 
+        [HttpPut]
+        public async Task<IActionResult> Put()
+        {
+            return Ok();
+        }
+
+        /// <summary> 
+        /// Serviço para exclusão / inativação de pedidos. 
+        /// </summary>
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
+
+        /// <summary> 
+        /// Serviço para consulta de pedidos. 
+        /// </summary> 
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
